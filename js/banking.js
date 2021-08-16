28a76c846cd8880e5b551c3218c5193fd5e27af2
alifbinhossain <you@example.com>
2021-08-17 05:46:34 +0600

    simple condition added

--- js/banking.js
+++ js/banking.js
@@ -28,19 +28,22 @@ document.getElementById("withdrawl-btn").addEventListener("click", function () {
   let withdrawField = document.getElementById("withdrawl-input");
   let newWithdrawAmount = parseFloat(withdrawField.value);
 
-  //update withdraw total
+  //update total withdraw  & balance
   let withdrawTotal = document.getElementById("withdraw-total");
   let previousWithdrawAmount = parseFloat(withdrawTotal.innerText);
-
   let currentWithdrawTotal = previousWithdrawAmount + newWithdrawAmount;
-  withdrawTotal.innerText = currentWithdrawTotal;
 
-  //update balance
   let balanceTotal = document.getElementById("balance-total");
   let previousBalance = parseFloat(balanceTotal.innerText);
 
   let newBalance = previousBalance - newWithdrawAmount;
-  balanceTotal.innerText = newBalance;
+  if (previousBalance > newWithdrawAmount) {
+    withdrawTotal.innerText = currentWithdrawTotal;
+    balanceTotal.innerText = newBalance;
+  } else {
+    alert(`You don't have ${newWithdrawAmount}$ in your account!`);
+    alert(`Your current balance is ${previousBalance}$.`);
+  }
 
   //clear withdraw field
   withdrawField.value = "";
