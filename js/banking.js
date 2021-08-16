0e962b562e3589395fd6fa09df7d85b6b55954e6
alifbinhossain <you@example.com>
2021-08-17 05:38:25 +0600

    button functionallity done

--- js/banking.js
+++ js/banking.js
@@ -21,3 +21,27 @@ document.getElementById("deposit-btn").addEventListener("click", function () {
   //clear deposit field
   depositField.value = "";
 });
+
+/* --------------------- WITHDRAW BUTTON FUNCTIONALLITY --------------------- */
+document.getElementById("withdrawl-btn").addEventListener("click", function () {
+  //get new withdraw amount
+  let withdrawField = document.getElementById("withdrawl-input");
+  let newWithdrawAmount = parseFloat(withdrawField.value);
+
+  //update withdraw total
+  let withdrawTotal = document.getElementById("withdraw-total");
+  let previousWithdrawAmount = parseFloat(withdrawTotal.innerText);
+
+  let currentWithdrawTotal = previousWithdrawAmount + newWithdrawAmount;
+  withdrawTotal.innerText = currentWithdrawTotal;
+
+  //update balance
+  let balanceTotal = document.getElementById("balance-total");
+  let previousBalance = parseFloat(balanceTotal.innerText);
+
+  let newBalance = previousBalance - newWithdrawAmount;
+  balanceTotal.innerText = newBalance;
+
+  //clear withdraw field
+  withdrawField.value = "";
+});
