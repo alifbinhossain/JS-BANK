888bb59337c54b0c8a858c4d171748e80eac4029
alifbinhossain <you@example.com>
2021-08-17 16:52:44 +0600

    common function added for reuse

--- js/banking.js
+++ js/banking.js
@@ -1,55 +1,67 @@
-/* ---------------------- DEPOSIT BUTTON FUNCTIONALLITY --------------------- */
-document.getElementById("deposit-btn").addEventListener("click", function () {
-  //get new deposit amount
-  let depositField = document.getElementById("deposit-input");
-  let newDepositAmount = parseFloat(depositField.value);
+/* ---------------------- COMMON FUNTIONALLITY FOR REUSE --------------------- */
+//store new amount
+function getNewAmount(transactionID) {
+  let inputField = document.getElementById(transactionID);
+  let newAmount = parseFloat(inputField.value);
+  //clear deposit field
+  inputField.value = "";
 
-  //update total deposit & total balance
-  let depositTotal = document.getElementById("total-deposit");
-  let previousDepositAmount = parseFloat(depositTotal.innerText);
-  let currentDepositAmount = previousDepositAmount + newDepositAmount;
+  return newAmount;
+}
 
+//update total
+function updateTotal(totalID, newAmount) {
+  let total = document.getElementById(totalID);
+  let previousAmount = parseFloat(total.innerText);
+  let currentAmount = previousAmount + newAmount;
+  total.innerText = currentAmount;
+}
+
+//update balance
+function updateBalance(newAmount, isADD) {
   let balanceTotal = document.getElementById("balance-total");
   let previousBalance = parseFloat(balanceTotal.innerText);
-  let newBalance = previousBalance + newDepositAmount;
-
-  //conditions to deposit money
-  if (newDepositAmount > 0 && isNaN(newDepositAmount) != true) {
-    depositTotal.innerText = currentDepositAmount;
+  if (isADD == true) {
+    let newBalance = previousBalance + newAmount;
     balanceTotal.innerText = newBalance;
+  } else {
+    let newBalance = previousBalance - newAmount;
+    balanceTotal.innerText = newBalance;
+  }
+}
+
+/* ---------------------- DEPOSIT BUTTON FUNCTIONALLITY --------------------- */
+document.getElementById("deposit-btn").addEventListener("click", function () {
+  //get new deposit amount & clear the field
+  let newDepositAmount = getNewAmount("deposit-input");
+
+  //update total deposit & total balance
+  if (newDepositAmount > 0 && isNaN(newDepositAmount) != true) {
+    updateTotal("total-deposit", newDepositAmount);
+    updateBalance(newDepositAmount, true);
   } else {
     alert(`Please enter valid amount to deposit your money!`);
   }
-  //clear deposit field
-  depositField.value = "";
 });
 
 /* --------------------- WITHDRAW BUTTON FUNCTIONALLITY --------------------- */
 document.getElementById("withdrawl-btn").addEventListener("click", function () {
-  //get new withdraw amount
-  let withdrawField = document.getElementById("withdrawl-input");
-  let newWithdrawAmount = parseFloat(withdrawField.value);
+  //get new withdraw amount & clear the field
+  let newWithdrawAmount = getNewAmount("withdrawl-input");
 
   //update total withdraw  & total balance
-  let withdrawTotal = document.getElementById("withdraw-total");
-  let previousWithdrawAmount = parseFloat(withdrawTotal.innerText);
-  let currentWithdrawTotal = previousWithdrawAmount + newWithdrawAmount;
-
-  let balanceTotal = document.getElementById("balance-total");
-  let previousBalance = parseFloat(balanceTotal.innerText);
-  let newBalance = previousBalance - newWithdrawAmount;
+  let previousBalance = parseFloat(
+    document.getElementById("balance-total").innerText
+  );
 
   //conditions to withdraw money
   if (previousBalance > newWithdrawAmount && newWithdrawAmount > 0) {
-    withdrawTotal.innerText = currentWithdrawTotal;
-    balanceTotal.innerText = newBalance;
+    updateTotal("withdraw-total", newWithdrawAmount);
+    updateBalance(newWithdrawAmount, false);
   } else if (isNaN(newWithdrawAmount) || newWithdrawAmount < 0) {
     alert(`Please enter valid amount to withdraw your money!`);
   } else {
     alert(`You don't have ${newWithdrawAmount}$ in your account!`);
     alert(`Your current balance is ${previousBalance}$.`);
   }
-
-  //clear withdraw field
-  withdrawField.value = "";
 });
